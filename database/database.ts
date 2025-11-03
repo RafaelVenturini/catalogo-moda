91e232ff31e31a066bcd4cf99dfd315b6e34b4c4
Rafael <rafaelventurinidipalma@gmail.com>
2025-11-03 14:03:04 -0300

    banco migrado para as nuvens

--- database/database.ts
+++ database/database.ts
@@ -1,11 +1,14 @@
 import { createPool } from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config();
 
 export const pool = createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'Mix210307*',
-	database: 'liss_moda',
-	port: 3306,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	port: 3307,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0
